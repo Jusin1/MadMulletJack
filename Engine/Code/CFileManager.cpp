3c65afc7d534a111fccc35d829e784b8a7fece35
KBJ_Laptop <dzzzing@naver.com>
2025-08-07 20:12:01 +0900

    docs : JsonFile

---
+++ Engine/Code/CFileManager.cpp
@@ -0,0 +1,16 @@
+#include "Engine_Define.h"
+#include "CFileManager.h"
+
+IMPLEMENT_SINGLETON(CFileManager)
+
+CFileManager::CFileManager()
+{
+}
+
+CFileManager::~CFileManager()
+{
+}
+
+void CFileManager::Free()
+{
+}
