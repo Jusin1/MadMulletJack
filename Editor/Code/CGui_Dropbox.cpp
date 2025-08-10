84474f3bdcf7dc26d217dcb749410799677bbf6b
Byungcoco <dzzzing@naver.com>
2025-08-11 00:53:29 +0900

    refactor : CGui_Dropbox

--- Editor/Code/CGui_Dropbox.cpp
+++ Editor/Code/CGui_Dropbox.cpp
@@ -1,20 +1,2 @@
 #include "pch.h"
 #include "CGui_Dropbox.h"
-
-CGui_Dropbox::CGui_Dropbox(string _label)
-	: CGuiBase(_label)
-{
-}
-
-CGui_Dropbox::~CGui_Dropbox()
-{
-}
-
-void CGui_Dropbox::Free()
-{
-}
-
-CGui_Dropbox *CGui_Dropbox::Create(string _label)
-{
-	return nullptr;
-}
--- Editor/Header/CGui_Dropbox.h
+++ Editor/Header/CGui_Dropbox.h
@@ -1,13 +1,71 @@
 #pragma once
 #include "CGuiBase.h"
+
+template<typename TEnum>
 class CGui_Dropbox : public CGuiBase
 {
 private:
-	explicit CGui_Dropbox(string _label);
+	explicit CGui_Dropbox(string _label, TEnum _eState, const vector<std::string> &_Names);
 	virtual ~CGui_Dropbox();
 
 	virtual void Free() override;
 public:
-	static CGui_Dropbox *Create(string _label);
+	static CGui_Dropbox *Create(string _label, TEnum _eState, const vector<std::string> &_Names);
+	virtual _bool Render() override;
+
+	TEnum GetState() const { return m_eState; }
+private:
+	TEnum m_eState;
+	vector<std::string> m_Names;
 };
 
+template<typename TEnum>
+inline CGui_Dropbox<TEnum>::CGui_Dropbox(string _label, TEnum _eState, const vector<std::string> &_Names)
+	: CGuiBae(_label), m_eState(_eState), m_Names(_Names)
+{
+}
+
+template<typename TEnum>
+inline CGui_Dropbox<TEnum>::~CGui_Dropbox()
+{
+}
+
+template<typename TEnum>
+inline void CGui_Dropbox<TEnum>::Free()
+{
+}
+
+template<typename TEnum>
+inline CGui_Dropbox<TEnum> *CGui_Dropbox<TEnum>::Create(string _label, TEnum _eState, const vector<std::string> &_Names)
+{
+	return new CGui_Dropbox<TEnum>(_label, _eState, _Names);
+}
+
+template<typename TEnum>
+inline _bool CGui_Dropbox<TEnum>::Render()
+{
+	_int curState{ static_cast<_int>(m_eState) };
+	_bool changed{ FALSE };
+	if (ImGui::BeginCombo(m_label.c_str(), m_names[curState]))
+	{
+		for (size_t i = 0; i < m_Names.size(); ++i)
+		{
+			_bool bSelected = (curState == i);
+			if (ImGui::Selectable(m_Names[i], bSelected))
+			{
+				if (curState != i)
+				{
+					m_state = static_cast<TEnum>(i);
+					changed = true;
+				}
+			}
+
+			if (bSelected)
+				ImGui::SetItemDefaultFocus();
+		}
+
+		ImGui::EndCombo();
+	}
+
+	return changed;
+}
