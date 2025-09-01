e06bb9e33cd6e3e45ab7fbe243df6bd383d50ecf
Hanung <jhwong1234@tukorea.ac.kr>
2025-09-01 12:48:25 +0900

    fixed : 몬스터 총알 버그 수정

--- Client/Code/CMonster_Suit.cpp
+++ Client/Code/CMonster_Suit.cpp
@@ -553,7 +553,7 @@ void CMonster_Suit::OnEnterState(MON_STATE s)
     case IDLE:  tag = L"Com_Texture_Idle";  break;
     case CHASE: tag = L"Com_Texture_Chase"; break;
     case AIM:   tag = L"Com_Texture_Aim";   break;
-    case SHOT:  m_shotTimer = 0.f; tag = L"Com_Texture_Shot"; break;
+    case SHOT:  m_shotTimer = 0.7f; tag = L"Com_Texture_Shot"; break;
     case JUMP:  tag = L"Com_Texture_Jump";  break;
     case HIT_ELECTRIC:
         tag = L"Com_Texture_Hit_Eletric";
@@ -652,7 +652,8 @@ void CMonster_Suit::OnUpdateState(MON_STATE s, const _float& dt)
         if (m_pPlayerTr) m_pTransformCom->LookAt(m_pPlayerTr->Get_Info(INFO_POS));
 
         m_shotTimer += dt;
-        if (m_pTextureCom->Is_AnimFinished()) m_pTextureCom->Stop_Anim();
+        if (m_pTextureCom->Is_AnimFinished())
+            m_pTextureCom->Stop_Anim();
 
         if (m_shotTimer >= 0.7f) {
             m_shotTimer = 0.f;
@@ -663,20 +664,15 @@ void CMonster_Suit::OnUpdateState(MON_STATE s, const _float& dt)
             tData.vMuzzlePosition = m_pTransformCom->Get_Info(INFO_POS);
             tData.vLookDir = m_pTransformCom->Get_Info(INFO_LOOK);
             D3DXVec3Normalize(&tData.vLookDir, &tData.vLookDir);
-            tData.vMuzzlePosition += tData.vLookDir * 2.f;
-            tData.vMuzzlePosition.y += 0.14f;
+            tData.vMuzzlePosition += tData.vLookDir * 1.2f;  
+            tData.vMuzzlePosition.y += 0.14f;               
+
+            _vec3 vDir = tData.vLookDir;
+            vDir.y -= 0.03f;  
+            D3DXVec3Normalize(&vDir, &vDir);
+            tData.vLookDir = vDir;
+
             CObjectPoolManager::GetInstance()->Spawn(PoolType::BULLET, &tData);
-            /*auto sceneIdx = CManagement::GetInstance()->Get_CurrentSceneIdx();
-            if (auto* bullet = dynamic_cast<CBullet*>(CObjectManager::GetInstance()->Clone_GameObject(
-                L"Prototype_GameObject_Bullet", sceneIdx, L"Monster_Layer")))
-            {
-                _vec3 vMuzzle = m_pTransformCom->Get_Info(INFO_POS);
-                _vec3 vLook = m_pTransformCom->Get_Info(INFO_LOOK);
-                D3DXVec3Normalize(&vLook, &vLook);
-                vMuzzle += vLook * 2.f;
-                vMuzzle.y += 0.14f;
-                bullet->Fire(vMuzzle, vLook);
-            }*/
         }
     }
     break;
--- Client/Code/CUIManager.cpp
+++ Client/Code/CUIManager.cpp
@@ -585,7 +585,7 @@ void CUIManager::DestroyEnterUI()
     // ¾À ±³Ã¼ (·Îµù¾ÀÀ¸·Î)
     LPDIRECT3DDEVICE9 pDev = CManagement::GetInstance()->GetCurrentScene()->GetDevice();
 
-    CManagement::GetInstance()->Open_Scene(SCENE_LOADING, CLoading_Scene::Create(pDev, SCENE_TUTORIAL));
+    CManagement::GetInstance()->Open_Scene(SCENE_LOADING, CLoading_Scene::Create(pDev, SCENE_CAR));
     ClearAllUI();
     m_exitingEnter = false; 
 }
