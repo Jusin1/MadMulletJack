c9489acd790d97f92fc0641eaefd9152fc1eaff8
Eunbi <lucyban@naver.com>
2025-09-05 02:45:06 +0900

    feat : minigun sound done

--- Client/Header/CMini_Gun.h
+++ Client/Header/CMini_Gun.h
@@ -29,7 +29,8 @@ private:
 
 	_float m_fScale;
 	_float m_fEffCoolTime;
-	_float m_fEffRenderTime;                                                
+	_float m_fEffRenderTime;              
+	_float m_fSoundCoolTime;
 
 public:
 	static CMini_Gun* Create(LPDIRECT3DDEVICE9 pGraphicDev);
